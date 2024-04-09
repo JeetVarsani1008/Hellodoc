2a049146757438fcab3b91642091bcf8be1af938
JeetVarsani1008 <tatva.dotnet.jeetvarsani@outlook.com>
2024-04-09 19:19:33 +0530

    update 38.0

--- HelloDoc2/obj/Debug/net7.0/HelloDoc2.AssemblyInfo.cs
+++ HelloDoc2/obj/Debug/net7.0/HelloDoc2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloDoc2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1449813836fb7eced35781d83313144aba0d3536")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c909342eeb9ca69fcc30ac67846f3594ee53311c")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloDoc2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloDoc2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
